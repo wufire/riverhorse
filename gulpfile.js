58755ff79466fa1df670ebb174c7add8c8fa9bbc
awu <wufire@gmail.com>
2017-06-06 02:33:31 -0500

    Node package files. Gulp file for deploying riverhorse to `wufire.com/dev/riverhorse`

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var ftp = require('vinyl-ftp');
+var gutil = require('gulp-util');
+var minimist = require('minimist');
+var args = minimist(process.argv.slice(2));
+
+gulp.task('deploy', function() {
+  var remotePath = '/dev/riverhorse';
+  var conn = ftp.create({
+    host: 'wufire.com',
+    user: args.user,
+    password: args.password,
+    log: gutil.log
+  });
+  gulp.src(['*.html', './assets/**', './app.js'], {base: '.'})
+    .pipe(conn.newer(remotePath))
+    .pipe(conn.dest(remotePath));
+})
